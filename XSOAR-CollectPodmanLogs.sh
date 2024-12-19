7b405c869dedccf17c6cfcd60ac988e8dd96c793
Chris Surline <chris.surline@gmail.com>
2024-12-19 10:15:09 -0500

    quick fix for podman rootless

--- XSOAR-CollectPodmanLogs.sh
+++ XSOAR-CollectPodmanLogs.sh
@@ -4,13 +4,13 @@ LOG_DIR="/var/log/demisto/container-logs" # where to save logfiles
 mkdir -p "$LOG_DIR"
 
 # get all container IDs
-CONTAINERS=$(podman ps -a -q)
+CONTAINERS=$(sudo -u demisto podman ps -a -q)
 
 TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
 
 for CONTAINER_ID in $CONTAINERS; do
     # use container name for logfile name if possible
-    CONTAINER_NAME=$(podman inspect -f '{{.Name}}' "$CONTAINER_ID" | sed 's/^\/\(.*\)/\1/')
+    CONTAINER_NAME=$(sudo -u demisto podman inspect -f '{{.Name}}' "$CONTAINER_ID" | sed 's/^\/\(.*\)/\1/')
 
     # if container name = empty, use container id
     if [[ -z "$CONTAINER_NAME" ]]; then
@@ -20,7 +20,7 @@ for CONTAINER_ID in $CONTAINERS; do
     fi
 
     # extract logs and save to logfile
-    podman logs "$CONTAINER_ID" &> "${LOG_DIR}/${FILENAME}"
+    sudo -u demisto podman logs "$CONTAINER_ID" &> "${LOG_DIR}/${FILENAME}"
 
     echo "Logs for container $CONTAINER_ID ($CONTAINER_NAME) saved to ${LOG_DIR}/${FILENAME}"
 done
